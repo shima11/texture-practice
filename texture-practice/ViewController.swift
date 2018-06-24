59a5c23f650fc78dd3e19ae8c86195e63dfc9c07
Shima <shima.jin@icloud.com>
2018-06-24 13:41:00 +0900

    wip

--- texture-practice/ViewController.swift
+++ texture-practice/ViewController.swift
@@ -7,14 +7,64 @@
 //
 
 import UIKit
+import AsyncDisplayKit
+
+class RootNode: ASDisplayNode {
+
+    let boxes = [
+    { ()->ASDisplayNode in
+        let node = ASDisplayNode()
+        node.backgroundColor = .lightGray
+        return node
+        }(),
+    { ()->ASDisplayNode in
+        let node = ASDisplayNode()
+        node.backgroundColor = .darkGray
+        return node
+        }()
+    ]
+
+    override init() {
+
+        super.init()
+
+        backgroundColor = .init(white: 0.95, alpha: 1)
+        boxes.forEach {
+            $0.style.height = .init(unit: .points, value: 40)
+            $0.style.flexBasis = .init(unit: .fraction, value: 0.5)
+        }
+
+        automaticallyManagesSubnodes = true
+    }
+
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+
+        return ASStackLayoutSpec(
+            direction: .horizontal,
+            spacing: 0,
+            justifyContent: .start,
+            alignItems: .start,
+            flexWrap: .wrap,
+            alignContent: .start,
+            lineSpacing: 0,
+            children: boxes
+        )
+    }
+
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = .white
+
+        let node = RootNode()
+        node.frame.size = .init(width: view.bounds.width, height: 100)
+        node.view.center = view.center
+        view.addSubnode(node)
     }
-
-
 }
 
