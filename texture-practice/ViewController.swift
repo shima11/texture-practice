b5d85ad0599c0c0ce11c912109892d8a689d3f03
Shima <shima.jin@icloud.com>
2018-06-24 12:50:48 +0900

    add project

---
+++ texture-practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  texture-practice
+//
+//  Created by jinsei shima on 2018/06/24.
+//  Copyright © 2018 jinsei shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
