8f2d5d1d688cbd1b3bc0379fd23eb9f3fcc4ac3a
Shima <shima.jin@icloud.com>
2020-04-17 13:22:30 +0900

    jikken

--- texture-practice/ViewController.swift
+++ texture-practice/ViewController.swift
@@ -30,10 +30,25 @@ class ViewController1: UIViewController {
         view.addSubview(transitionView)
         view.addSubview(button)
         
+        button.node.setTitle("Toggle", with: nil, with: .black, for: .normal)
+        
         transitionView.easy.layout(Edges())
-            
+
+        button.easy.layout(
+            CenterX(),
+            Bottom(60)
+        )
+        
+        button.node.addTarget(self, action: #selector(didTapButton), forControlEvents: .touchUpInside)
         
     }
+    
+    @objc func didTapButton() {
+        
+        transitionView.node.isOpen.toggle()
+        transitionView.node.transitionLayout(withAnimation: true, shouldMeasureAsync: false, measurementCompletion: nil)
+    }
+
 }
 
 
@@ -42,12 +57,133 @@ class TransitionNode: ASDisplayNode {
     let node1 = ASDisplayNode()
     let node2 = ASDisplayNode()
 
+    var isOpen: Bool = false
+    
+    override init() {
+        super.init()
+        
+        automaticallyManagesSubnodes = true
+        
+        node1.backgroundColor = .blue
+        node2.backgroundColor = .red
+        
+    }
     
     override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
-        LayoutSpec {
+
+//        node1.style.preferredSize = .init(width: 100, height: 10)
+//        node2.style.preferredSize = .init(width: 200, height: 100)
+//
+//        node1.style.maxSize = .init(width: 100, height: 10)
+
+//        let tmpNode: ASDisplayNode
+//        if isOpen {
+//            tmpNode = node1
+//        } else {
+//            tmpNode = node2
+//        }
+
+        
+        return LayoutSpec {
             VStackLayout {
-                node1
-                node2
+                VSpacerLayout()
+//                tmpNode
+                if isOpen {
+                    HStackLayout {
+                        HSpacerLayout()
+                        node1.preferredSize(.init(width: 100, height: 10))
+                    }
+                } else {
+                    node2.preferredSize(.init(width: 200, height: 100))
+                }
+                VSpacerLayout()
+            }
+        }
+    }
+
+    override func animateLayoutTransition(_ context: ASContextTransitioning) {
+
+        print("hoge:", node1.frame, node2.frame)
+        print("hoge:init:", context.initialFrame(for: node1), context.initialFrame(for: node2))
+        print("hoge:init:", context.finalFrame(for: node1), context.finalFrame(for: node2))
+
+        if isOpen {
+            
+//            self.node1.alpha = 0
+//            self.node2.alpha = 1
+//
+//            self.node1.frame = context.initialFrame(for: node2)
+//            self.node2.frame = context.initialFrame(for: node2)
+            
+            UIView.animate(withDuration: 0.3, delay: 0, options: [.beginFromCurrentState, .allowUserInteraction], animations: {
+
+                self.node1.alpha = 1
+                self.node2.alpha = 0
+
+                self.node1.frame = context.finalFrame(for: self.node1)
+                self.node2.frame = context.finalFrame(for: self.node1)
+                
+            }, completion: { finished in
+                context.completeTransition(finished)
+            })
+
+        } else {
+
+//            self.node1.alpha = 1
+//            self.node2.alpha = 0
+//
+//            self.node1.frame = context.initialFrame(for: node1)
+//            self.node2.frame = context.initialFrame(for: node1)
+            
+            UIView.animate(withDuration: 0.3, delay: 0, options: [.beginFromCurrentState, .allowUserInteraction], animations: {
+                
+                self.node1.alpha = 0
+                self.node2.alpha = 1
+                
+                self.node1.frame = context.finalFrame(for: self.node2)
+                self.node2.frame = context.finalFrame(for: self.node2)
+                
+            }, completion: { finished in
+                context.completeTransition(finished)
+            })
+
+        }
+
+    }
+    
+}
+
+class TransitionNode2: ASDisplayNode {
+        
+    let node1 = ASDisplayNode()
+    let node2 = ASDisplayNode()
+
+    var isOpen: Bool = false
+    
+    override init() {
+        super.init()
+        
+        automaticallyManagesSubnodes = true
+        
+        node1.backgroundColor = .blue
+        node2.backgroundColor = .red
+        
+    }
+    
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        
+        return LayoutSpec {
+            VStackLayout {
+                VSpacerLayout()
+                if isOpen {
+                    HStackLayout {
+                        HSpacerLayout()
+                        node1.preferredSize(.init(width: 100, height: 10))
+                    }
+                } else {
+                    node2.preferredSize(.init(width: 200, height: 100))
+                }
+                VSpacerLayout()
             }
         }
     }
