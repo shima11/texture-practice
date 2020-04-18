c1fefb872885dc84aaf0aa14f017778ee51ead67
Shima <shima.jin@icloud.com>
2020-04-19 02:12:29 +0900

    update

--- texture-practice/ViewController.swift
+++ texture-practice/ViewController.swift
@@ -16,249 +16,304 @@ import TextureBridging
 import TextureSwiftSupport
 
 class ViewController1: UIViewController {
-
-    let bodyView = NodeView.init(node: BodyNode())
-    override func viewDidLoad() {
-
-        super.viewDidLoad()
-
-        view.backgroundColor = .white
-        
-        view.addSubview(bodyView)
-        
-        bodyView.easy.layout(Edges())
-    }
+  
+  let bodyView = NodeView.init(node: BodyNode())
+  override func viewDidLoad() {
     
+    super.viewDidLoad()
+    
+    view.backgroundColor = .white
+    
+    view.addSubview(bodyView)
+    
+    bodyView.easy.layout(Edges())
+  }
 }
 
 
 class BodyNode: ASDisplayNode {
+  
+  let transitionNode = TransitionNode()
+  
+  let button = ASButtonNode()
+  
+  override init() {
+    super.init()
+    
+    automaticallyManagesSubnodes = true
+    
+    button.setTitle("Toggle", with: nil, with: .black, for: .normal)
+    
+    button.addTarget(self, action: #selector(didTapButton), forControlEvents: .touchUpInside)
+    
+  }
+  
+  @objc func didTapButton() {
+    
+    transitionNode.isOpen.toggle()
 
-    let scrollNode = ASScrollNode()
-    
-    let contentNode = ASDisplayNode()
-    
-    let transitionNode = TransitionNode()
-    
-    let button = ASButtonNode()
-    
-    var bottomHeight: CGFloat = 100 {
-        didSet {
-            bottomSpacer.style.height = .init(unit: .points, value: bottomHeight)
+    transitionNode.transitionLayout(withAnimation: true, shouldMeasureAsync: false, measurementCompletion: nil)
+  }
+  
+  
+  override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+    LayoutSpec {
+      ZStackLayout {
+        
+        transitionNode
+        
+        VStackLayout {
+          VSpacerLayout()
+          button
+          VSpacerLayout()
         }
+      }
     }
-    
-    let bottomSpacer = ASDisplayNode()
-
-    override init() {
-        super.init()
-        
-        
-        automaticallyManagesSubnodes = true
-        
-        scrollNode.automaticallyManagesSubnodes = true
-        scrollNode.automaticallyManagesContentSize = true
-        
-        scrollNode.view.alwaysBounceVertical = true
-        
-        bottomSpacer.style.height = .init(unit: .points, value: bottomHeight)
-        
-        button.setTitle("Toggle", with: nil, with: .black, for: .normal)
-        
-        button.addTarget(self, action: #selector(didTapButton), forControlEvents: .touchUpInside)
-        
-        contentNode.automaticallyManagesSubnodes = true
-        
-//        contentNode.layoutSpecBlock = { _, _ in
-//            LayoutSpec {
-//                VStackLayout {
-//                    VSpacerLayout()
-//                    self.transitionNode
-//                    self.bottomSpacer
-//                }
-//            }
-//        }
-        
-        scrollNode.layoutSpecBlock = { _, _ in
-//            LayoutSpec {
-//                self.contentNode
-//            }
-            LayoutSpec {
-                VStackLayout {
-                    VSpacerLayout()
-                    self.transitionNode
-                    self.bottomSpacer
-                }
-            }
-
-        }
-    }
-    
-    @objc func didTapButton() {
-        
-        transitionNode.isOpen.toggle()
-        bottomHeight = transitionNode.isOpen ? 300 : 100
-
-        contentNode.setNeedsLayout()
-        scrollNode.setNeedsLayout()
-
-//        scrollNode.transitionLayout(withAnimation: true, shouldMeasureAsync: false, measurementCompletion: nil)
-        
-        transitionNode.transitionLayout(withAnimation: true, shouldMeasureAsync: false, measurementCompletion: nil)
-    }
-
-    
-    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
-        LayoutSpec {
-            ZStackLayout {
-                scrollNode
-                
-                VStackLayout {
-                    VSpacerLayout()
-                    button
-                    VSpacerLayout()
-                }
-            }
-        }
-    }
+  }
 }
 
 
 class TransitionNode: ASDisplayNode {
-        
-    let node1 = ASDisplayNode()
-    let node2 = ASDisplayNode()
-
-    var isOpen: Bool = false
+  
+  let contentNode = ContentNode()
+  let bottomSpacer = ASDisplayNode()
+  
+  let backgoundNode = ASDisplayNode()
+  
+  let scrollNode = ASScrollNode()
+  
+  private var bottomHeight: CGFloat = 200 {
+    didSet {
+      bottomSpacer.style.height = .init(unit: .points, value: bottomHeight)
+    }
+  }
+  
+  var isOpen: Bool = false {
+    didSet {
+      contentNode.isOpen = isOpen
+      bottomHeight = isOpen ? 400 : 200
+    }
+  }
+  
+  override init() {
+    super.init()
     
-    override init() {
-        super.init()
-        
-        automaticallyManagesSubnodes = true
-        
-        node1.backgroundColor = .blue
-        node2.backgroundColor = .red
-        
+    automaticallyManagesSubnodes = true
+    scrollNode.automaticallyManagesSubnodes = true
+    scrollNode.automaticallyManagesContentSize = true
+    
+    scrollNode.view.alwaysBounceVertical = true
+    
+    scrollNode.layoutSpecBlock = { _, _ in
+      LayoutSpec {
+        self.backgoundNode
+      }
     }
     
-    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
-
-//        node1.style.preferredSize = .init(width: 100, height: 10)
-//        node2.style.preferredSize = .init(width: 200, height: 100)
-//
-//        node1.style.maxSize = .init(width: 100, height: 10)
-
-//        let tmpNode: ASDisplayNode
-//        if isOpen {
-//            tmpNode = node1
-//        } else {
-//            tmpNode = node2
-//        }
-
-        
-        return LayoutSpec {
-            VStackLayout {
-                VSpacerLayout()
-//                tmpNode
-                if isOpen {
-                    HStackLayout {
-                        HSpacerLayout()
-                        node1.preferredSize(.init(width: 100, height: 10))
-                    }
-                } else {
-                    node2.preferredSize(.init(width: 200, height: 100))
-                }
-                VSpacerLayout()
-            }
+    backgoundNode.automaticallyManagesSubnodes = true
+    backgoundNode.backgroundColor = .lightGray
+    backgoundNode.layoutSpecBlock = { _, _ in
+      LayoutSpec {
+        VStackLayout {
+          VSpacerLayout()
+          self.contentNode
+          self.bottomSpacer
         }
+      }
     }
+    
+  }
+  
+  override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+    LayoutSpec {
+      scrollNode
+    }
+  }
+  
+//  func updateLayout() {
+//        print("hoge:", contentNode.node1.supernode, contentNode.node2.supernode)
+//        print("hoge:", contentNode.node1.frame, contentNode.node2.frame)
+//
+//    self.contentNode.setNeedsLayout()
+//    self.contentNode.layoutIfNeeded()
+//    self.contentNode.setLayout()
+////    self.contentNode.setNeedsLayout()
+////    self.scrollNode.setNeedsLayout()
+//
+//        UIView.animate(withDuration: 0.3, animations: {
+//          self.layoutIfNeeded()
+//        }, completion: { finished in
+//
+//        })
+//
+//  }
+  
+  override func animateLayoutTransition(_ context: ASContextTransitioning) {
+            
+//    print("hoge:", contentNode.node1.supernode, contentNode.node2.supernode)
+//    print("hoge:", contentNode.node1.frame, contentNode.node2.frame)
 
-    override func animateLayoutTransition(_ context: ASContextTransitioning) {
+    self.bottomSpacer.setNeedsLayout()
+    
+//    self.backgoundNode.setNeedsLayout()
+//    self.scrollNode.setNeedsLayout()
+    
+    contentNode.transitionLayout(withAnimation: true, shouldMeasureAsync: false, measurementCompletion: nil)
 
-        print("hoge:", node1.frame, node2.frame)
-        print("hoge:init:", context.initialFrame(for: node1), context.initialFrame(for: node2))
-        print("hoge:init:", context.finalFrame(for: node1), context.finalFrame(for: node2))
+    UIView.animate(withDuration: 0.3, animations: {
+//      self.contentNode.setLayout()
+//      self.layoutIfNeeded()
+      self.backgoundNode.layoutIfNeeded()
+    }, completion: { finished in
+      context.completeTransition(finished)
+    })
+    
+  }
+  
+}
 
+
+class ContentNode: ASDisplayNode {
+  
+  let node1 = ASDisplayNode()
+  let node2 = ASDisplayNode()
+  
+  var isOpen: Bool = false
+  
+  override init() {
+    super.init()
+    
+    automaticallyManagesSubnodes = true
+    
+    node1.backgroundColor = .blue
+    node2.backgroundColor = .red
+    
+  }
+  
+  override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+    
+    return LayoutSpec {
+      VStackLayout {
+        VSpacerLayout()
         if isOpen {
-            
-//            self.node1.alpha = 0
-//            self.node2.alpha = 1
-//
-//            self.node1.frame = context.initialFrame(for: node2)
-//            self.node2.frame = context.initialFrame(for: node2)
-            
-            UIView.animate(withDuration: 0.3, delay: 0, options: [.beginFromCurrentState, .allowUserInteraction], animations: {
-
-                self.node1.alpha = 1
-                self.node2.alpha = 0
-
-                self.node1.frame = context.finalFrame(for: self.node1)
-                self.node2.frame = context.finalFrame(for: self.node1)
-                
-            }, completion: { finished in
-                context.completeTransition(finished)
-            })
-
+          HStackLayout {
+            HSpacerLayout()
+            node1.preferredSize(.init(width: 100, height: 10))
+          }
         } else {
-
-//            self.node1.alpha = 1
-//            self.node2.alpha = 0
+          node2.preferredSize(.init(width: 200, height: 100))
+        }
+        VSpacerLayout()
+      }
+    }
+  }
+  
+//  func setLayout() {
 //
-//            self.node1.frame = context.initialFrame(for: node1)
-//            self.node2.frame = context.initialFrame(for: node1)
-            
-            UIView.animate(withDuration: 0.3, delay: 0, options: [.beginFromCurrentState, .allowUserInteraction], animations: {
-                
-                self.node1.alpha = 0
-                self.node2.alpha = 1
-                
-                self.node1.frame = context.finalFrame(for: self.node2)
-                self.node2.frame = context.finalFrame(for: self.node2)
-                
-            }, completion: { finished in
-                context.completeTransition(finished)
-            })
-
-        }
-
-    }
+//    let frame1 = self.node1.frame
+//    let frame2 = self.node2.frame
+//
+//    if isOpen {
+//
+//      self.node1.alpha = 1
+//      self.node2.alpha = 0
+//
+//      self.node1.frame = frame1
+//      self.node2.frame = frame1
+//
+//    } else {
+//
+//      self.node1.alpha = 0
+//      self.node2.alpha = 1
+//
+//      self.node1.frame = frame2
+//      self.node2.frame = frame2
+//    }
+//  }
+  
+  override func animateLayoutTransition(_ context: ASContextTransitioning) {
     
+    print("hoge:", node1.frame, node2.frame)
+    print("hoge:init:", context.initialFrame(for: node1), context.initialFrame(for: node2))
+    print("hoge:init:", context.finalFrame(for: node1), context.finalFrame(for: node2))
+    
+    if isOpen {
+      
+      self.node1.alpha = 0
+      self.node2.alpha = 1
+      
+      self.node1.frame = context.initialFrame(for: node2)
+      self.node2.frame = context.initialFrame(for: node2)
+      
+      UIView.animate(withDuration: 0.3, delay: 0, options: [.beginFromCurrentState, .allowUserInteraction], animations: {
+        
+        self.node1.alpha = 1
+        self.node2.alpha = 0
+        
+        self.node1.frame = context.finalFrame(for: self.node1)
+        self.node2.frame = context.finalFrame(for: self.node1)
+        
+      }, completion: { finished in
+        context.completeTransition(finished)
+      })
+      
+    } else {
+      
+      self.node1.alpha = 1
+      self.node2.alpha = 0
+      
+      self.node1.frame = context.initialFrame(for: node1)
+      self.node2.frame = context.initialFrame(for: node1)
+      
+      UIView.animate(withDuration: 0.3, delay: 0, options: [.beginFromCurrentState, .allowUserInteraction], animations: {
+        
+        self.node1.alpha = 0
+        self.node2.alpha = 1
+        
+        self.node1.frame = context.finalFrame(for: self.node2)
+        self.node2.frame = context.finalFrame(for: self.node2)
+        
+      }, completion: { finished in
+        context.completeTransition(finished)
+      })
+    }
+  }
 }
 
-class TransitionNode2: ASDisplayNode {
-        
-    let node1 = ASDisplayNode()
-    let node2 = ASDisplayNode()
-
-    var isOpen: Bool = false
-    
-    override init() {
-        super.init()
-        
-        automaticallyManagesSubnodes = true
-        
-        node1.backgroundColor = .blue
-        node2.backgroundColor = .red
-        
-    }
-    
-    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
-        
-        return LayoutSpec {
-            VStackLayout {
-                VSpacerLayout()
-                if isOpen {
-                    HStackLayout {
-                        HSpacerLayout()
-                        node1.preferredSize(.init(width: 100, height: 10))
-                    }
-                } else {
-                    node2.preferredSize(.init(width: 200, height: 100))
-                }
-                VSpacerLayout()
-            }
-        }
-    }
-    
-}
+//class TransitionNode2: ASDisplayNode {
+//
+//    let node1 = ASDisplayNode()
+//    let node2 = ASDisplayNode()
+//
+//    var isOpen: Bool = false
+//
+//    override init() {
+//        super.init()
+//
+//        automaticallyManagesSubnodes = true
+//
+//        node1.backgroundColor = .blue
+//        node2.backgroundColor = .red
+//
+//    }
+//
+//    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+//
+//        return LayoutSpec {
+//            VStackLayout {
+//                VSpacerLayout()
+//                if isOpen {
+//                    HStackLayout {
+//                        HSpacerLayout()
+//                        node1.preferredSize(.init(width: 100, height: 10))
+//                    }
+//                } else {
+//                    node2.preferredSize(.init(width: 200, height: 100))
+//                }
+//                VSpacerLayout()
+//            }
+//        }
+//    }
+//
+//}
 
